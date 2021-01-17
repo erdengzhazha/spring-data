ff62619b829b771580c59807cf842ac916949b9b
Xiu_Er <13813641925@163.com>
2021-01-17 19:36:01 +0800

    补全提交

---
+++ spring-data-elasticsearch/src/main/java/com/ovopark/springdata/elasticsearch/SpringdataApplication.java
@@ -0,0 +1,13 @@
+package com.ovopark.springdata.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdataApplication.class, args);
+    }
+
+}
---
+++ spring-data-elasticsearch/src/test/java/com/ovopark/springdata/elasticsearch/SpringdataApplicationTests.java
@@ -0,0 +1,25 @@
+package com.ovopark.springdata.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+
+@SpringBootTest
+class SpringdataApplicationTests {
+
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
+
+    @Test
+    public  void t1(){
+        boolean test01 = elasticsearchTemplate.createIndex("test01");
+        System.out.println(test01);
+    }
+
+    @Test
+    public void t(){
+        System.out.println("sss");
+    }
+
+}
